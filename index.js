7de7f6ceb716d434f7364143bb59c599c36e8eac
Anthony <kabuthuantony@gmail.com>
2024-04-09 12:43:16 +0300

    Fetch

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  // Send a fetch request to 'https://anapioficeandfire.com/api/books'
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json(); // Parse the JSON response
+    })
+    .then(data => {
+      renderBooks(data); // Call renderBooks with the retrieved data
+    })
+    .catch(error => {
+      console.error('Error fetching books:', error);
+    });
 }
 
 function renderBooks(books) {
